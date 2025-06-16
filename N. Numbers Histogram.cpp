bac63df9f6223d0f3734fd064372f495e16b72fb
Ashraful Alam <ashrafulalamyt@gmail.com>
2025-06-16 23:41:45 +0600

    Add implementation of the "Numbers Histogram" program in C++

---
+++ N. Numbers Histogram.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define tc ll t;cin>>t;while(t--)
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main()
+{
+    fast;
+    char c;
+    cin >> c;
+    tc{
+        ll n;
+        cin >> n;
+        for (ll i = 0; i < n; i++)
+            cout << c;
+        cout << endl;
+    }
+    return 0;
+}
